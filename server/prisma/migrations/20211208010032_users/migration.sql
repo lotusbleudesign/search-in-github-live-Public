77c561ab6652bb3d94ea48e22da5c0ea3055f74b
AliceH <47507206+lotusbleudesign@users.noreply.github.com>
2021-12-08 11:37:25 +0100

    Delete server/prisma/migrations directory

--- server/prisma/migrations/20211208010032_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "hireable" TEXT,
-ALTER COLUMN "company" DROP NOT NULL,
-ALTER COLUMN "email" DROP NOT NULL,
-ALTER COLUMN "twitter_username" DROP NOT NULL;
--- server/prisma/migrations/20211208012619_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "User_login_key";
-
--- DropIndex
-DROP INDEX "User_node_id_key";
